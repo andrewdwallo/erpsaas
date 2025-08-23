7cb9f0fd0367c3cf7c15d3105acb92a8b9047783
Andrew Wallo <andrewdwallo@gmail.com>
2025-08-23 15:14:19 -0400

    wip

--- resources/views/filament/infolists/components/report-entry.blade.php
+++ resources/views/filament/infolists/components/report-entry.blade.php
@@ -1,21 +1,8 @@
 <x-dynamic-component :component="$getEntryWrapperView()" :entry="$entry">
-    @php
-        $url = $getUrl();
-        $shouldOpenInNewTab = $shouldOpenUrlInNewTab();
-    @endphp
-
-    @if(filled($url))
-        <a href="{{ $url }}"@if($shouldOpenInNewTab) target="_blank" rel="noopener noreferrer"@endif>
-    @endif
-
     <x-report-entry
         :heading="$getHeading()"
         :description="$getDescription()"
         :icon="$getIcon()"
         :iconColor="$getIconColor()"
     />
-
-    @if(filled($url))
-        </a>
-    @endif
 </x-dynamic-component>
